4b951c92296a8bed1e7dbc19ed3b91b7fb3fe43a
MariuszMackowski <m.mackowski@op.pl>
2020-01-31 18:51:49 +0100

    Translated to eng

--- Apriori/apriori.py
+++ Apriori/apriori.py
@@ -11,189 +11,168 @@ import matplotlib.pyplot as plt
 import scipy.io as sio
 import time
 
-def binaryzacja(X):
-    for i in range(0,len(X)):
-        for j in range(0,len(X[i])):
-            if(X[i][j] > 0):
-                X[i][j] = 1
-    return X
 
-def supp(args,dane):
-    s = 0    
-    for i in range(dane.shape[0]):
+def support(args,data):
+    """   
+    TO UPGRADE 
+   
+    """
+    sup = 0    
+    for i in range(data.shape[0]):
         row = []
         for arg in args:
-            row.append(dane[i][arg-1])        
+            row.append(data[i][arg-1])        
         if all(list(map(lambda x: x == 1,row))):
-            s+=1        
-    return s
+            sup+=1        
+    return sup
 
-def wyznaczanie_regul(zbiory_czeste):
-    # print('Otrzymalem zbiory: ',zbiory_czeste)
-    reguly = list()
-    print('================ MOZLIWE REGULY ================')
-    for zbior in zbiory_czeste:
-        for zbiory in zbiory_czeste:  
-            
-            if zbior_w_zbiorze(zbior,zbiory):            
-                print_regula(zbior,zbiory)
-                reguly.append( (zbior,zbiory ))
-    return reguly
+def determine_rules(frequent_sets):  
+    rules = list()
+    print('================ POSSIBLE RULES ================')
+    for sett in frequent_sets:
+        for sets in frequent_sets:            
+            if set_in_set(sett,sets):            
+                print_rule(sett,sets)
+                rules.append( (sett,sets) )
+    return rules
 
-def print_regula(zbior1,zbior2):
+def print_rule(set1,set2):
     s = '{'
-    for z in zbior1:
-        s+='A'+str(z)+','
+    for z in set1:
+        s+=str(z)+','
     s = s[:-1] + '} -> {'
-    for z in zbior2:
-        if z not in zbior1:
-            s += 'A'+str(z)+','
+    for z in set2:
+        if z not in set1:
+            s += str(z)+','
     s = s[:-1] + '}'
     print(s)
 
-def zbior_w_zbiorze(zbior1,zbior2):    
-    if zbior1 == zbior2 or type(zbior1)==int:        
+def set_in_set(set1,set2):    
+    if set1 == set2 or type(set1)==int:        
         return False
-    for i in zbior1:
-        if i not in zbior2:            
+    for i in set1:
+        if i not in set2:            
             return False
     return True
 
-def zbiory_czeste(wsparcie,minSupp):    
-    return filter(lambda x: wsparcie[x]>=minSupp,wsparcie)
+def frequent_sets(support,minSupp):    
+    return filter(lambda x: support[x]>=minSupp,support)
       
-def podzbiory(klucze,i):
-    return itertools.combinations(klucze,i)
+def subsets(keys,size):
+    return itertools.combinations(keys,size)
 
-def wyznacz_zbiory(dane,minWsparcie):
-    zbiory = []
-    wsparcie = dict()
-    keys = list(range(1,dane.shape[1]+1))
-    for i in range(1,dane.shape[1]+1):
-        print('====================',i,'====================')
-        pdzbiory = podzbiory(dict.fromkeys(keys),i)    
-        wsp = dict.fromkeys(pdzbiory,0)        
-        for k in wsp.keys():
-            wsp[k] = supp(k,dane)
-            
-        # print('Wsp przed del:\n',wsp)
-        for k in wsp.copy().keys():
-            # print('Element key: ',k,' value: ',wsp[k])
-            if wsp[k] < minWsparcie:
-                del wsp[k]
-        print('Wsp:\n ',wsp)
-        if wsp == {}:
-            print('BRAK PODZBIOROW O ROZMIARZE ',i,'O ZAUFANIU WIEKSZYM NIZ MINIMALNE \n'
-                  '=================== PRZERYWAM ===================\n')
-            
+def determine_sets(data,minSupport):
+    sets = []
+    supp = dict()
+    keys = list(range(1,data.shape[1]+1))
+    for i in range(1,data.shape[1]+1):
+        print('==================== SIZE ',i,' ====================')
+        sbsets = subsets(dict.fromkeys(keys),i)    
+        su = dict.fromkeys(sbsets,0)
+        for k in su.keys():
+            su[k] = support(k,data)            
+        
+        for k in su.copy().keys():            
+            if su[k] < minSupport:
+                del su[k]
+        print('Frequent sets:\n ',su)
+        if su == {}:
+            print('NO SUBSET OF SIZE ',i,'WITH SUPPORT HIGHER THEN MINIMAL \n'
+                  '=================== BREAKING ===================\n')            
             break
-        wsparcie.update(wsp)
-        # print('Wsparcie:\n ',wsparcie)
-        zb = list(zbiory_czeste(wsp,minWsparcie))
-        for z in zb:
-            if z not in zbiory:
-                zbiory.append(z)
-            
-        # print('Zb:\n ',zb)
-        keys = list(dict.fromkeys(list(itertools.chain(*zb))))
-        # print('Zbiory:\n ',zbiory)        
-        print('Keys:\n ',keys)    
-    return [zbiory,wsparcie]
+        supp.update(su)        
+        fs = list(frequent_sets(su,minSupport))
+        for z in fs:
+            if z not in sets:
+                sets.append(z)            
+        keys = list(dict.fromkeys(list(itertools.chain(*fs))))            
+    return [sets,supp]
 
-def zaufanie(reguly,wsparcie):
-    zauf = dict()    
-    for tup in reguly:
-        zauf[tup] = wsparcie[tup[1]]/wsparcie[tup[0]]        
-    return zauf
+def confidence(rules,support):
+    conf = dict()    
+    for tup in rules:
+        conf[tup] = support[tup[1]]/support[tup[0]]        
+    return conf
 
-def minimalne_zaufanie(zaufanie,min_zaufanie):
-    return filter(lambda x: zaufanie[x]>min_zaufanie,zaufanie)
+def minimal_confidence(conf,min_support):
+    return filter(lambda x: conf[x]>min_support,conf)
 
-def dane_do_wykresu(ostateczne_reguly,wsparcie,zaufanie):
-    wsp = []
-    zauf = []
-    for regula in ostateczne_reguly:
-        wsp.append(wsparcie[regula[1]])
-        zauf.append(zaufanie[regula])
-    return [wsp,zauf]
+def graph_data(final_rules,support,convidence):
+    supp = []
+    conv = []
+    for rule in final_rules:
+        supp.append(support[rule[1]])
+        conv.append(convidence[rule])
+    return [supp,conv]
 
-def wykres(dane,maxX):    
-    plt.plot(dane[0],dane[1],'k.')
+def graph(data,maxX):    
+    plt.plot(data[0],data[1],'k.')
     plt.xlim(left=0,right=maxX)
     plt.ylim(bottom=0,top=1.1)
     
-def brzeg_pareto(dane):
-    brzeg = dict()         
-    dane = przetworz_dane(dane)      
-    maxWsparcie = max(dane.keys())   
+def pareto_edge(data):
+    edge = dict()         
+    data = process_data(data)      
+    maxWsparcie = max(data.keys())   
     m = 0    
     for i in range(maxWsparcie,0,-1):        
-        if i in dane.keys() and dane[i]>m:
-            brzeg[i] = dane[i]
-            m = dane[i]
-    plt.plot(list(brzeg.keys()),list(brzeg.values()),'r.')    
-    keys = list(brzeg.keys())    
+        if i in data.keys() and data[i]>m:
+            edge[i] = data[i]
+            m = data[i]
+    plt.plot(list(edge.keys()),list(edge.values()),'r.')    
+    keys = list(edge.keys())    
     for i, k  in enumerate(keys):         
         if i == 0:
-            plt.plot([k,k],[0,brzeg[k]],':r')
-        if i == len(brzeg.items())-1:
-            plt.plot([k,0],[brzeg[k],brzeg[k]],':r')
-        if i<len(brzeg.items())-1:
-            plt.plot([keys[i+1],k],[brzeg[keys[i]],brzeg[keys[i]]],':r')
-            plt.plot([keys[i+1],keys[i+1]],[brzeg[keys[i]],brzeg[keys[i+1]]],':r')
-    return brzeg
+            plt.plot([k,k],[0,edge[k]],':r')
+        if i == len(edge.items())-1:
+            plt.plot([k,0],[edge[k],edge[k]],':r')
+        if i<len(edge.items())-1:
+            plt.plot([keys[i+1],k],[edge[keys[i]],edge[keys[i]]],':r')
+            plt.plot([keys[i+1],keys[i+1]],[edge[keys[i]],edge[keys[i+1]]],':r')
+    return edge
                 
-def przetworz_dane(dane):    
-    wynik = dict.fromkeys(dane[0],0)    
-    for i in range(len(dane[0])):        
-        if wynik[dane[0][i]]<dane[1][i]:
-            wynik[dane[0][i]] = dane[1][i]
-    return wynik
+def process_data(data):    
+    result = dict.fromkeys(data[0],0)    
+    for i in range(len(data[0])):        
+        if result[data[0][i]]<data[1][i]:
+            result[data[0][i]] = data[1][i]
+    return result
 
 
-def apriori(dane,minWsparcie,minZaufanie):
-    start = time.time()
-    [zbiory,wsparcie] = wyznacz_zbiory(dane,minWsparcie)    
-    reguly = wyznaczanie_regul(zbiory)
+def apriori(data,minSupport,minConvidance):
+    start_time = time.time()
+    [sets,supports] = determine_sets(data,minSupport)    
+    rules = determine_rules(sets)
     # print('Reguly:\n ',reguly)
-    zauf = zaufanie(reguly,wsparcie)    
-    ostateczne_reguly = list(minimalne_zaufanie(zauf,minZaufanie))
-    print(' ================ OSTATECZNE REGULY ================')
-    for r in ostateczne_reguly:
-        print_regula(r[0],r[1])
-        print('Zaufanie:',round(zauf[r],4))
-        print('Wsparcie: ',round(wsparcie[r[1]]))
+    conv = confidence(rules,supports)    
+    final_rules = list(minimal_confidence(conv,minConvidance))
+    print(' ================ FINAL RULES ================')
+    for r in final_rules:
+        print_rule(r[0],r[1])
+        print('Convidence:',round(conv[r],4))
+        print('Support: ',round(supports[r[1]]))
     #wykres
-    dane_wykres = dane_do_wykresu(ostateczne_reguly,wsparcie,zauf)
+    graph_data_prepared = graph_data(final_rules,supports,conv)
     
-    wykres(dane_wykres,1.1*max(dane_wykres[0]))
-    brzeg = brzeg_pareto(dane_wykres)
-    print(' ================ REGULY NA BRZEGU ================')    
-    for k,v in zauf.items():
-        for b in brzeg:
-            if v == brzeg[b]:
-                print_regula(k[0], k[1])
-                print('Zaufanie: ',brzeg[b])
-                print('Wsparcie: ',b)
+    graph(graph_data_prepared,1.1*max(graph_data_prepared[0]))
+    edge = pareto_edge(graph_data_prepared)
+    print(' ================ RULES ON APRIORI\'S EDGE ================')    
+    for k,v in conv.items():
+        for b in edge:
+            if v == edge[b]:
+                print_rule(k[0], k[1])
+                print('Convidence: ',edge[b])
+                print('Support: ',b)
     
-    plt.title('Wykres dla zbioru z '+str(dane.shape[1])+' atrybutami dla:\n minimalnego wsparcia: '+str(minWsparcie)+' \n minimalnego zaufania: '+str(minZaufanie))
+    plt.title('Aprori graph for data set with '+str(data.shape[1])+' attributes\n minimal support: '+str(minSupport)+' \n minimal convidence: '+str(minConvidance))
     plt.show()
-    print('================ CZAS ================')
-    print("Czas wykonywania: ",round(time.time()-start,2),' s')
-    print("Ilosc zbiorow istotnych: ",len(ostateczne_reguly))
+    print('================ TIME ================')
+    print("Processing time : ",round(time.time()-start_time,2),' s')
+    print("Amount of frequent sets: ",len(final_rules))
     
-""" Przyklad z pdf
-#A1 = [0, 1, 0, 1, 1, 1, 0, 0, 0, 1, 1, 1, 0, 0, 0, 0, 1, 1, 1, 0]
-#A2 = [1, 1, 0, 1, 1, 0, 0, 0, 0, 0, 1, 0, 1, 1, 1, 1, 0, 1, 0, 0]
-#A3 = [1, 0, 0, 1, 0, 0, 1, 1, 1, 0, 0, 0, 1, 0, 0, 0, 1, 1, 1, 1]
-#A4 = [1, 0, 1, 0, 1, 0, 0, 1, 1, 0, 1, 1, 1, 1, 1, 0, 1, 1, 1, 1]
-#A = [A1,A2,A3,A4]
-#A = np.transpose(np.array(A))
-#apriori(A,6,0.5)
-"""
 
 
-
-reuters = sio.loadmat('reuters.mat')
-reuters = reuters['TOPICS'][:1000,:]
-apriori(reuters,2,0.6)
+if __name__ == "__main__":
+    reuters = sio.loadmat('reuters.mat')
+    reuters = reuters['TOPICS'][:1000,:]
+    apriori(reuters,15,0.6)
